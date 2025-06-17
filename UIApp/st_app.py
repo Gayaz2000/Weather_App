fa4ebf1464b59e8387b7a6b37e3a00a2c9b3207b
Gayaz2000 <gayazjune2000@gmail.com>
2025-06-17 20:22:53 +0530

    weather agent completed

---
+++ UIApp/st_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import requests
+
+st.title("🔗 Weather App")
+
+city = st.text_area("Enter your prompt")
+WEATHER_API_KEY = st.text_input("Enter your openweather api key", type="password") #type="password"
+
+if st.button("Get Weather"):
+    if city and WEATHER_API_KEY:
+        with st.spinner("Waiting for LLM response..."):
+            response = requests.post(
+                "http://127.0.0.1:8000/llm/infer",
+                json={"city": city, "WEATHER_API_KEY":WEATHER_API_KEY}
+            )
+            if response.status_code == 200:
+                result = response.json()["response"]
+                st.success(result)
+            else:
+                st.error("Failed to get response from LLM.")
---
+++ endpoints_api/myapp.py
@@ -0,0 +1,22 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+import httpx 
+from MyAgentFolder.newagent import weather_agent
+
+app = FastAPI()
+
+# Input schema
+class LLMRequest(BaseModel):
+    city: str
+    WEATHER_API_KEY : str
+
+# Simulated response function — replace with your model call
+async def run_weather_agent(city: str, WEATHER_API_KEY:str) -> str:
+    output = weather_agent({"city":city, "WEATHER_API_KEY":WEATHER_API_KEY})
+    return output["messages"][-1].content
+
+# FastAPI POST endpoint
+@app.post("/chat")
+async def infer(request: LLMRequest):
+    output = await run_weather_agent(request.city, request.WEATHER_API_KEY)
+    return {"response": output}
